d4700c58740ab3eb376f632b70b21f1ef4b7ff01
christaanna <christaannasabu@gmail.com>
2025-06-20 15:21:11 +0530

    Initial commit

---
+++ src/main/java/org/example/employee.java
@@ -0,0 +1,165 @@
+package org.example;
+
+import com.mongodb.client.*;
+import com.mongodb.client.model.*;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.lt;
+import static com.mongodb.client.model.Updates.combine;
+import static com.mongodb.client.model.Updates.set;
+
+
+public class employee {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MongoClient client = MongoClients.create("mongodb://admin:admin@172.21.14.92:27017,172.21.17.53:27017,172.21.17.54:27017/");
+        MongoDatabase database = client.getDatabase("EmployeeDetails");
+        MongoCollection<Document> collection = database.getCollection("employee");
+        int choice;
+
+        do {
+            System.out.println("1.Add \n 2.Update \n 3.Delete \n 4.Search \n 5.List \n 6.department statistics");
+            System.out.println("\n Enter your choice : ");
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 1 :
+                    System.out.println("Enter the number of employees you want to insert :");
+                    int n = sc.nextInt();
+                    sc.nextLine();
+
+                    List<Document> stud = new ArrayList<>();
+                    for (int i=0; i<n ;i++) {
+                        System.out.println("Enter Employee name : ");
+                        String name = sc.nextLine();
+                        System.out.println("Enter EmailId : ");
+                        String email = sc.nextLine();
+                        if (collection.find(eq("email", email)).first() != null) {
+                            System.out.println("Error: Email " + email + " already exists. Skipping this entry.");
+                            continue;
+                        }
+//                        System.out.println("Enter phone number: ");
+//                        int phone = sc.nextInt();
+//                        sc.nextLine();
+                        System.out.println("Enter Skill : ");
+                        String skill = sc.nextLine();
+                        System.out.println("Enter Department : ");
+                        String department = sc.nextLine();
+                        System.out.println("Enter Joining Date : ");
+                        String jod = sc.nextLine();
+
+                        Document doc = new Document("name", name).append("email", email).append("skills", skill).append("department", department).append("jod", jod);
+                        stud.add(doc);
+                    }
+                    collection.insertMany(stud);
+                    break;
+
+
+
+                case 2 :
+                    System.out.println("Enter Employee name to update: ");
+                    String name = sc.nextLine();
+                    Document name1 = collection.find(new Document("name", name)).first();
+
+                    if (name1 == null) {
+                        System.out.println("Employee with name " + name+ " not found.");
+                        break;
+                    }
+                    System.out.println("Enter updated Skill : ");
+                    String skill = sc.nextLine();
+                    System.out.println("Enter updated Department : ");
+                    String department = sc.nextLine();
+
+
+                    Bson filter = eq("name",name);
+                    Bson update = combine(set("skills", skill),set("department",department));
+                    FindOneAndUpdateOptions options = new FindOneAndUpdateOptions()
+                            .returnDocument(ReturnDocument.AFTER);
+                    Document updatedDoc = collection.findOneAndUpdate(filter, update,options);
+                    if (updatedDoc != null) {
+                        System.out.println("\nUpdated Employee:");
+                        System.out.println(updatedDoc.toJson());
+                    } else {
+                        System.out.println("No document found to update.");
+                    }
+
+                    break;
+
+                case 3 :
+                    System.out.println("Enter EmailId : ");
+                    String email = sc.nextLine();
+                    Document docs = collection.find(new Document("email", email)).first();
+                    if (docs != null) {
+                        collection.deleteOne(eq("email",email));
+                        System.out.println("\nDeleted Employee:");
+                        System.out.println(docs.toJson());
+                    } else {
+                        System.out.println("No document found to update.");
+                    }
+
+                    break;
+
+                case 4 :
+                    System.out.println("Enter field to search by (name/email/department/skills): ");
+                    String searchField = sc.nextLine();
+
+                    System.out.println("Enter value to search: ");
+                    String searchValue = sc.nextLine();
+
+                    FindIterable<Document> results = collection.find(eq(searchField, searchValue));
+                    for (Document doc : results) {
+                        System.out.println(doc.toJson());
+                    }
+                    break;
+
+                case 5 :
+                    int page = 1;
+                    int pageSize = 5;
+                    System.out.println("Sort by: 1) Name  2) Joining Date");
+                    int sortOption = sc.nextInt();
+
+                    Bson sortField;
+                    if (sortOption == 1) {
+                        sortField = Sorts.ascending("name");
+                    } else {
+                        sortField = Sorts.ascending("joiningDate");
+                    }
+
+                    FindIterable<Document> allEmployees = collection.find().sort(sortField).skip((page - 1) * pageSize).limit(pageSize);
+                    for (Document doc : allEmployees) {
+                        System.out.println(doc.toJson());
+                    }
+                    break;
+
+                case 6 :
+
+                    List<Bson> pipeline = List.of(
+                            new Document("$group", new Document("_id", "$department")
+                                    .append("count", new Document("$sum", 1)))
+                    );
+                    collection.aggregate(pipeline).forEach(doc -> {
+                        System.out.println("Department: " + doc.getString("_id") + ", Count: " + doc.getInteger("count"));
+                    });
+                    break;
+
+                default:
+                    System.out.println("Invalid choice.");
+            }
+
+
+        }while(choice != 6);
+        client.close();
+    }
+}
+
+
+
+
+
